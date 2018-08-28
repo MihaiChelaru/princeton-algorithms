9a7ed94b879da7041411d3bb0e04bdeb26df6d2e
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-08-27 20:23:18 -0400

    added API methods

---
+++ Deques_and_Randomized_Queues/src/Deque.java
@@ -0,0 +1,37 @@
+public class Deque implements Iterable<Item> {
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst() {
+
+    }
+
+    public Item removeLast() {
+
+    }
+
+    public Iterator<Item> iterator() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
