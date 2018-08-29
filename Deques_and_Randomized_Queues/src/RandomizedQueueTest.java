f88cee230402983efb9eb9a09fa059bb949c9066
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-08-29 16:29:01 -0400

    RandomizedQueue tests added and passed

---
+++ Deques_and_Randomized_Queues/src/RandomizedQueueTest.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class RandomizedQueueTest {
+    @Test()
+    public void randomizedQueueConstructorCreatesEmptyDeque() {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        Assert.assertEquals(queue.size(), 0);
+        Assert.assertTrue(queue.isEmpty());
+    }
+
+    @Test()
+    public void enqueueTest() {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        Assert.assertEquals(queue.size(), 2);
+    }
+
+    @Test()
+    public void dequeueTest() {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        queue.enqueue(1);
+        int item1 = queue.dequeue();
+        queue.enqueue(2);
+        int item2 = queue.dequeue();
+        Assert.assertEquals(item1, 1);
+        Assert.assertEquals(item2, 2);
+        Assert.assertTrue(queue.isEmpty());
+    }
+
+    @Test()
+    public void sampleTest() {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.sample();
+        Assert.assertEquals(queue.size(), 2);
+    }
+}
