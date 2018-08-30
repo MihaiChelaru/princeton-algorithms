5f251dfa33b6e71e9a4d5e6524f5ac24894a3235
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-08-30 19:44:44 -0400

    added collinear point classes, method stubs, and javadocs

---
+++ Collinear_Points/src/BruteCollinearPoints.java
@@ -0,0 +1,23 @@
+public class BruteCollinearPoints {
+
+    /**
+     * Finds all line segments containing 4 or more points
+     */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * Returns number of line segments
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * Returns the line segments containing 4 or more points
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
